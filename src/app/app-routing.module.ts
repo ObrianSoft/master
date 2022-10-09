0452afc10ab338e8cfa93f1228d75f8d0117296f
Anon <anon@whatever>
2022-10-09 19:36:21 +0200

    Init with timerdetail, functions and checks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
+import { TimerDetailComponent } from './components/timer/timer-detail/timer-detail.component';
 import { TimerOverviewComponent } from './components/timer/timer-overview/timer-overview.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   // {path: <base-path>, component: <component>, outlet: <target_outlet_name>}
-  {path: '', component: TimerOverviewComponent},
+  {path: '', component: TimerDetailComponent},
   {path: 'timer', component: TimerOverviewComponent},
   {path: 'timer/detail', component: TimerOverviewComponent},
 ];
